7abb65068ea20680cd9824d7764fa9306f419e0e
ChrisStedman <christopher.john.stedman@gmail.com>
2024-11-15 09:38:56 +1100

    Add Issues loading skeleton

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -4,35 +4,32 @@ import Link from 'next/link'
 import React from 'react'
 import { getIssues } from '../lib/supabase/issues/Issues';
 import { IssueStatusBadge } from '../components/IssueStatusBadge';
+import IssuesToolbar from './IssuesToolbar';
 
 export default async function Issues() {
   const issues: Issue[] = await getIssues()
 
   return (
     <div>
-      <div className='mb-5'>
-        <Button>
-            <Link href='issues/new' >New Issue</Link>
-        </Button>
-      </div>
-        <Table.Root variant='surface'>
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+      <IssuesToolbar />
+      <Table.Root variant='surface'>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map(issue => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>{issue.title}</Table.Cell>
+              <Table.Cell><IssueStatusBadge status={issue.status} /></Table.Cell>
+              <Table.Cell>{new Date(issue.created_at).toLocaleString('en-AU')}</Table.Cell>
             </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {issues.map(issue => (
-              <Table.Row key={issue.id}>
-                <Table.Cell>{issue.title}</Table.Cell>
-                <Table.Cell><IssueStatusBadge status={issue.status} /></Table.Cell>
-                <Table.Cell>{new Date(issue.created_at).toLocaleString('en-AU')}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table.Root>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   )
 }
