e7a06103c612804c62248c1d7f8afc77746f78ec
ChrisStedman <christopher.john.stedman@gmail.com>
2024-11-14 14:03:47 +1100

    Handle issue submission. Uses api route and action

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -1,12 +1,7 @@
+import { createIssueSchema } from "@/schemas/IssueSchemas";
 import { Issue } from "@/types/issues";
 import { createClient } from "@/utils/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from 'zod';
-
-const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
-})
 
 export async function POST(request: NextRequest) {
     const body = await request.json()
--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,13 +1,20 @@
 'use client';
+
 import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+import React, { useActionState } from 'react'
+import { submit } from './actions';
 
 export default function page() {
+  const [state, action, isPending] = useActionState(submit, undefined)
+
   return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root placeholder="Title" />
-        <TextArea placeholder='Description' />
-        <Button>Submit New Issue</Button>
-    </div>
+    <form action={action} className='max-w-xl space-y-3'>
+        <TextField.Root name='title' placeholder="Title" />
+        {state?.errors?.title && <p className="text-red-500 mt-2">{state.errors.title}</p>}
+        <TextArea name='description' placeholder='Description' />
+        {state?.errors?.description && <p className="text-red-500 mt-2">{state.errors.description}</p>}
+        <Button type='submit' disabled={isPending}>Submit New Issue</Button>
+        {state?.serverError && <p className="text-red-500 mt-2">{state.serverError}</p>}
+    </form>
   )
 }
